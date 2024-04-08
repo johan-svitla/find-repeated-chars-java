1e78a676aee648d8265dccebb81f0a3648a44965
Johan <jmarchan@gmail.com>
2024-04-08 16:06:54 -0300

    Solution 1

--- Main.java
+++ Main.java
@@ -8,9 +8,20 @@ public class Main {
     System.out.println("Input: " + input1 + ", Unique characters: " + uniqueCharacters(input1));
     System.out.println("Input: " + input2 + ", Unique characters: " + uniqueCharacters(input2));
     System.out.println("Input: " + input3 + ", Unique characters: " + uniqueCharacters(input3));
+     
   }
   public static String uniqueCharacters(String input) {
-    return "!";
+    StringBuilder result = new StringBuilder();
+    boolean[] visited = new boolean[26]; // Assuming only lowercase English letters
+
+    for (char c : input.toCharArray()) {
+        if (!visited[c - 'a']) { // If the character hasn't been visited before
+            result.append(c);
+            visited[c - 'a'] = true; // Mark the character as visited
+        }
+    }
+
+    return result.toString();
   }
 
 }
